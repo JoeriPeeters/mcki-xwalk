0d3218bb0debbd9159741b4a351bf2d38f2e8b1b
Joeri_Peeters <joeri_peeters@epam.com>
2026-08-11 23:13:55 +0200

    add subelem

--- blocks/media-viewer/media-viewer.js
+++ blocks/media-viewer/media-viewer.js
@@ -2,24 +2,5 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 import './media-viewer-component.js';
 
 export default function decorate(block) {
-  const items = [...block.children];
 
-  // leeg blok: laat UE's eigen placeholder/instrumentatie ongemoeid,
-  // zodat de "+"-affordance kan verschijnen
-  if (items.length === 0) return;
-
-  const viewer = document.createElement('media-viewer');
-  moveInstrumentation(block, viewer); // block-level UE-tracking meenemen naar het nieuwe element
-
-  items.forEach((item) => {
-    const img = item.querySelector('img');
-    if (!img) return;
-
-    const wrapper = document.createElement('span');
-    moveInstrumentation(item, wrapper);
-    wrapper.append(img);
-    viewer.append(wrapper);
-  });
-
-  block.replaceChildren(viewer);
 }
