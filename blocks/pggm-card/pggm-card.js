c8983694aba35560f879016d1d30343a75ac3654
Joeri Peeters <joeri.peeters@pggm.nl>
2026-08-12 14:44:12 +0200

    ignore vendor shizz

--- blocks/pggm-card/pggm-card.js
+++ blocks/pggm-card/pggm-card.js
@@ -1,7 +1,8 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  // 1. laadt p-elements-core → zet CustomElement/Maquette/CustomElementConfig/property op globalThis
+  // 1. laadt p-elements-core → zet
+  // CustomElement/Maquette/CustomElementConfig/property op globalThis
   await import('./vendor/p-elements-core.js');
 
   // 2. fix de naam-mismatch (card.js verwacht 'Property', p-elements-core zet 'property')
