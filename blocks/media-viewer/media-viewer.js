4441008016477019445497fd189b14d34eea0b48
Joeri_Peeters <joeri_peeters@epam.com>
2026-08-12 10:01:23 +0200

    mediaviewer: add time

--- blocks/media-viewer/media-viewer.js
+++ blocks/media-viewer/media-viewer.js
@@ -1,12 +1,17 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 import './media-viewer-component.js';
+import 'https://esm.sh/@github/relative-time-element';
 
 export default function decorate(block) {
   const greeting = block.querySelector('p')?.textContent?.trim() || 'Hello Web component !';
 
   const viewer = document.createElement('media-viewer');
-  viewer.dataset.greeting = greeting; // → wordt data-greeting="..." in de DOM
+  viewer.dataset.greeting = greeting;
   moveInstrumentation(block, viewer);
+
+  const timeEl = document.createElement('relative-time');
+  timeEl.setAttribute('datetime', new Date(Date.now() - 2 * 24 * 60 * 60 * 1000).toISOString());
+  viewer.append(timeEl);
 
   block.replaceChildren(viewer);
 }
